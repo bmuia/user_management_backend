043d977f5b20111432862fdaf8636e4607b3dcc5
bmuia <belammuia0@gmail.com>
2025-05-06 12:16:29 +0300

    bug(bug) fix bug

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,9 +1,8 @@
 # urls.py
 from django.urls import path
-from .views import ContactAdminView, AdminMessageListView,AdminRespondMessageView
+from .views import ContactAdminView, AdminMessageListView
 
 urlpatterns = [
     path('contact-admin/', ContactAdminView.as_view(), name='contact-admin'),
     path('admin/messages/', AdminMessageListView.as_view(), name='admin-messages'),
-    path('admin/reply/', AdminRespondMessageView.as_view(), name='admin-respond-message'),
 ]
