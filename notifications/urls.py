55e95712fa3d750c8e972be4d9023f769df11a42
bmuia <belammuia0@gmail.com>
2025-05-06 12:02:32 +0300

    feat implement admin messages where admin can see messages from users

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,8 +1,9 @@
 # urls.py
 from django.urls import path
-from .views import ContactAdminView, AdminMessageListView
+from .views import ContactAdminView, AdminMessageListView,AdminRespondMessageView
 
 urlpatterns = [
     path('contact-admin/', ContactAdminView.as_view(), name='contact-admin'),
     path('admin/messages/', AdminMessageListView.as_view(), name='admin-messages'),
+    path('admin/reply/', AdminRespondMessageView.as_view(), name='admin-respond-message'),
 ]
