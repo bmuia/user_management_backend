b71198f85e081f235bbf0dbe1a5fca6667ffc98d
bmuia <belammuia0@gmail.com>
2025-05-08 23:43:21 +0300

    bug database fix

--- user_management_backend/settings.py
+++ user_management_backend/settings.py
@@ -21,7 +21,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ENV = os.getenv('ENV')
+ENV = os.getenv('ENVIRONMENT')
 
 
 # Quick-start development settings - unsuitable for production
