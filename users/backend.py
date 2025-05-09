a85f0a7e8128c0ac31bd455040723fe6407b866a
bmuia <belammuia0@gmail.com>
2025-05-09 21:59:41 +0300

    bugs

--- users/backend.py
+++ users/backend.py
@@ -1,13 +1,16 @@
-from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
-
-User = get_user_model()
+from django.contrib.auth import get_user_model
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, email=None, password=None, **kwargs):
+        UserModel = get_user_model()
         try:
-            user = User.objects.get(email=email)
-            if user.check_password(password) and self.user_can_authenticate(user):
+            user = UserModel.objects.get(email__iexact=email)
+            if user.check_password(password):
                 return user
-        except User.DoesNotExist:
+            else:
+                return None
+        except UserModel.DoesNotExist:
+            return None
+        except Exception as e:
             return None
